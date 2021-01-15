3aa7265893e5e4715c6dc81b986b8b972bd1e81c
yanxx <yanxiangxiang2997@dingtalk.com>
2021-01-15 17:29:56 +0800

    回显

--- resources/views/file.blade.php
+++ resources/views/file.blade.php
@@ -13,8 +13,24 @@
             <div class="upload_add_btn Js_upload_warp">
                 <?php 
                     $imgs = (strpos(old($column, $value), $host) !== false) ? old($column, $value) : $host . '/' . old($column, $value);
+                    $exts = explode('.',$imgs);
+                    $ext = strtolower($exts[count($exts)-1]);
+                    if(in_array($ext,['jpg','jpeg','png','gif'])){
+                        $type = 1;//图片
+                    }elseif(in_array($ext,['mp4','flv','wmv','rmvb','mpeg','avi','mov','3gp'])){
+                        $type = 2;
+                    }else{
+                        $type = 3;
+                    }
                 ?>
-                <img data-filename="{{old($column, $value)}}" src="{{$imgs}}?x-oss-process=image/resize,m_fill,w_100,h_100">
+                @if($type==1)
+                    <img data-filename="{{old($column, $value)}}" src="{{$imgs}}?x-oss-process=image/resize,m_fill,w_100,h_100">
+                @elseif($type==2)
+                    <video data-filename="{{old($column, $value)}}" src="{{$imgs}}?x-oss-process=video/snapshot,t_4000,f_jpg,w_240,h_135,m_fast" controls="" preload="none"></video>
+                @else
+                    
+                @endif
+
                 <div class="upload_model" onclick="del_pic(this,false)">删除</div>
                 <div class="upload_add_img" id="{{$column}}_upload" style="position: relative; z-index: 1; display: none;">+</div>
                 <input type="hidden" class="Js_upload_input" name="{{$column}}" value="{{old($column, $value)}}">
